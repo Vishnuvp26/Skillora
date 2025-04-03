8f146804f9947cd7251817efb470c549fc931d60
Vishnu Mohan <vishnumohanmfc@gmail.com>
2025-04-03 16:51:02 +0530

    fix: testimonial marquee effect

--- Frontend/src/components/landing/Body.tsx
+++ Frontend/src/components/landing/Body.tsx
@@ -5,6 +5,7 @@ import image1 from '../../assets/image1.jpg';
 import image2 from '../../assets/image2.jpg';
 import image3 from '../../assets/image3.jpg';
 import landing from '../../assets/landing.jpg'
+import { Button } from "../ui/button";
 
 const Body = () => {
     return (
@@ -87,12 +88,12 @@ const Body = () => {
                     <h2 className="text-3xl lg:text-4xl font-bold text-gray-900 dark:text-white leading-[1.3]">
                         Up your work game, it’s easy
                     </h2>
-                    <ul className="mt-8 space-y-6 text-lg"> {/* Increased spacing here */}
+                    <ul className="mt-8 space-y-6 text-lg">
                         <li className="flex items-start space-x-3">
                             <CheckCircle className="text-[#0077B6] dark:text-[#00FFE5] w-7 h-7" />
                             <div>
                                 <h3 className="text-gray-900 dark:text-white text-xl font-bold ">No cost to join</h3>
-                                <h3 className="text-gray-800 dark:text-gray-300 text-sm">
+                                <h3 className="text-gray-800 dark:text-gray-400 text-sm">
                                     Register and browse talent profiles, explore projects.
                                 </h3>
                             </div>
@@ -101,8 +102,8 @@ const Body = () => {
                             <Briefcase className="text-[#0077B6] dark:text-[#00FFE5] w-7 h-7" />
                             <div>
                                 <h3 className="text-gray-900 dark:text-white text-xl font-bold">Post a job and hire top talent</h3>
-                                <h3 className="text-gray-800 dark:text-gray-300 text-sm">
-                                    Finding talent is easy—post a job or let us search for you!
+                                <h3 className="text-gray-800 dark:text-gray-400 text-sm">
+                                    Finding talent is easy post a job or let us search for you!
                                 </h3>
                             </div>
                         </li>
@@ -110,15 +111,20 @@ const Body = () => {
                             <DollarSign className="text-[#0077B6] dark:text-[#00FFE5] w-7 h-7" />
                             <div>
                                 <h3 className="text-gray-900 dark:text-white text-xl font-bold">Work without breaking the bank</h3>
-                                <h3 className="text-gray-800 dark:text-gray-300 text-sm">
+                                <h3 className="text-gray-800 dark:text-gray-400 text-sm">
                                     Makes it affordable to up your work.
                                 </h3>
                             </div>
                         </li>
                     </ul>
-                    <button className="mt-6 px-5 py-3 bg-[#0077B6] dark:bg-[#00FFE5] text-white dark:text-black font-semibold rounded-md shadow-md hover:bg-[#005F8C] dark:hover:bg-[#00BFA5] transition text-base">
-                        Get started
-                    </button>
+                    <div className="mt-6 flex justify-center md:justify-start">
+                        <Button className="px-5 py-3 border border-[#0077B6] text-[#0077B6] bg-transparent 
+                hover:bg-[#0077B611] hover:text-[#0077B6] 
+                dark:border-[#00FFE5] dark:text-[#00FFE5] dark:hover:bg-[#00FFE511] dark:hover:text-[#00FFE5] rounded-lg">
+                            Get started
+                        </Button>
+                    </div>
+
                 </motion.div>
             </motion.div>
 
